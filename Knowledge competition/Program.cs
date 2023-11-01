e14518b09840f5430c70a002021cafa162e5c12e
Hasan Yıldız <hy0782617@gmail.com>
2023-11-01 15:14:07 +0300

    Knowledge competition
    A simple three-question quiz in C# programming language, each question consisting of four options.
    
    (Her biri dört seçenekten oluşan, C# programlama dilinde basit üç soruluk bir sınav.)

---
+++ Knowledge competition/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+
+namespace bilgiYarismasi
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Bilgi Yarışmasına Hoş geldiniz ---");
+            Console.WriteLine();
+            Console.WriteLine("-- Kurallar -- ");
+            Console.WriteLine();
+            Console.WriteLine("-> Yarışmamız 3 sorudan oluşmaktadır");
+            Console.WriteLine("-> Her soru 4 şıktan oluşmaktadır");
+            Console.WriteLine("-> Soruyu bilemezseniz diğer soruya geçemezsiniz");
+            Console.WriteLine("   ve yarışmanız sonlanır");
+            Console.WriteLine();
+            Console.WriteLine("------------------------------------------------------");
+            Console.ReadKey();
+            Console.WriteLine();
+
+            soru1 soru1 = new soru1();
+            soru1.Questions();
+            Console.WriteLine("");
+            Console.Write("Cevabınız: ");
+            string cevap1 = Console.ReadLine();
+
+
+            switch (cevap1)
+            {
+                case "A":
+                    Console.WriteLine("Cevabınız Yanlış");
+                    Console.WriteLine();
+                    Console.WriteLine("Tekrar Deneyiniz...");
+                    Console.ReadKey();
+                    break;
+                case "C":
+                    Console.WriteLine("Cevabınız Yanlış");
+                    Console.WriteLine();
+                    Console.WriteLine("Tekrar Deneyiniz...");
+                    Console.ReadKey();
+                    break;
+                case "D":
+                    Console.WriteLine("Cevabınız Yanlış");
+                    Console.WriteLine();
+                    Console.WriteLine("Tekrar Deneyiniz...");
+                    Console.ReadKey();
+                    break;
+                
+            }
+
+            if (cevap1 != "B")
+            {
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Doğru Cevap");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine();
+
+            soru2 soru2 = new soru2();
+            soru2.Questions();
+            Console.WriteLine("");
+            Console.Write("Cevabınız: ");
+            string cevap2 = Console.ReadLine();
+
+            switch (cevap2)
+            {
+                case "A":
+                    Console.WriteLine("Cevabınız Yanlış");
+                    Console.WriteLine();
+                    Console.WriteLine("Tekrar Deneyiniz...");
+                    Console.ReadKey();
+                    break;
+                case "B":
+                    Console.WriteLine("Cevabınız Yanlış");
+                    Console.WriteLine();
+                    Console.WriteLine("Tekrar Deneyiniz...");
+                    Console.ReadKey();
+                    break;
+                case "D":
+                    Console.WriteLine("Cevabınız Yanlış");
+                    Console.WriteLine();
+                    Console.WriteLine("Tekrar Deneyiniz...");
+                    Console.ReadKey();
+                    break;
+
+            }
+
+            if (cevap2 != "C")
+            {
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Doğru Cevap");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine();
+
+            soru3 soru3 = new soru3();
+            soru3.Questions();
+            Console.WriteLine("");
+            Console.Write("Cevabınız: ");
+            string cevap3 = Console.ReadLine();
+
+            switch (cevap3)
+            {
+                case "B":
+                    Console.WriteLine("Cevabınız Yanlış");
+                    Console.WriteLine();
+                    Console.WriteLine("Tekrar Deneyiniz...");
+                    Console.WriteLine();
+                    Console.ReadKey();
+                    break;
+                case "C":
+                    Console.WriteLine("Cevabınız Yanlış");
+                    Console.WriteLine();
+                    Console.WriteLine("Tekrar Deneyiniz...");
+                    Console.WriteLine();
+                    Console.ReadKey();
+                    break;
+                case "D":
+                    Console.WriteLine("Cevabınız Yanlış");
+                    Console.WriteLine();
+                    Console.WriteLine("Tekrar Deneyiniz...");
+                    Console.WriteLine();
+                    Console.ReadKey();
+                    break;
+
+            }
+
+            if (cevap3 != "A")
+            {
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Doğru Cevap");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine();
+
+            Console.WriteLine("--- YARIŞMAYI BAŞARIYLA BİTİRDİNİZ ---");
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
---
+++ Knowledge competition/sorular.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bilgiYarismasi
+{
+    internal abstract class sorular
+    {
+        abstract public void Questions();
+    }
+
+    internal class soru1 : sorular
+    {
+        public override void Questions()
+        {
+            Console.WriteLine("1.Soru: Türkiyenin Başkenti Neresidir?");
+            Console.WriteLine("");
+            Console.WriteLine("A) İstanbul B) Ankara  C) İzmir  D) Diyarbakır");
+        }
+    }
+
+    internal class soru2 : sorular
+    {
+        public override void Questions()
+        {
+            Console.WriteLine("2. Soru: İstanbulun  Plaka Kodu Kaçtır?");
+            Console.WriteLine();
+            Console.WriteLine("A) 21  B)36  C)34  D)10");
+        }
+    }
+
+    internal class soru3 : sorular
+    {
+        public override void Questions()
+        {
+            Console.WriteLine("3. Ateşten Gömlek Kimin Eseridir?");
+            Console.WriteLine();
+            Console.WriteLine("A) Sagopa Kajmer  B) Halide Edip Adıvar");
+            Console.WriteLine("C) Nâzım Hikmet  D) Necip Fazıl Kısakürek");
+        }
+    }
+}
